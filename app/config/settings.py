b2dd066d32d94b6ea4a670a1e0f4b48170e88b7a
sangmin <sang93423@gmail.com>
2018-07-05 16:39:15 +0900

    HTTP요청을 받는 uWSGI설정파일 추가, Pipfile에 uwsgi 추가

--- app/config/settings.py
+++ app/config/settings.py
@@ -14,11 +14,16 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 # STATICFILES
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (STATIC_DIR,)
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
 
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
